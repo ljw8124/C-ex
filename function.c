41a24de55c132fe9f1461dd7a28b6eaad7ba21d2
이정우 <ljw8124@gmail.com>
2023-03-28 12:55:16 +0900

    함수의 개념

---
+++ function.c
@@ -0,0 +1,56 @@
+#include <stdio.h>   // 입출력 함수를 처리하기 위한 헤더파일
+#include <math.h>    // 수학 함수를 처리하기 위한 헤더파일
+#include <string.h>  // 문자열 처리함수를 위한 헤더파일
+#include <ctype.h>   // 문자형태 판별함수를 위한 헤더파일
+
+int exFunc_ex03(int a, int b);  // 함수의 원형선언
+// 숫자
+void exFunc_01() {
+    double x = 12.34;
+    int i = -5, j = 2;
+    int a, b, c;
+
+    a = ceil(x);
+    b = floor(x);
+    c = pow(4, j);
+    printf("abs(-5) = %d\n", abs(i));
+    printf("cos(10) = %f\n", cos(10));
+    printf("ceil(12.34) = %f\n", a);
+    printf("floor(12.34) = %d\n", b);
+    printf("pow(4, 2) = %d\n", c);  // 역승의 값을 출력
+}
+
+// 문자열
+void exFunc_ex02() {
+    int i, alp = 0, no = 0, et = 0;
+    char s[20];
+    printf("String");
+    scanf("%s", s);
+    for(i = 0; i < strlen(s); i++) {   // 문자열 길이를 구하는 함수
+        if(isalpha(s[i])) {             // 영문자 여부를 구하는 함수
+            alp++;
+        } else if(isdigit(s[i])) {      // 숫자 여부를 구하는 함수
+            no++;
+        } else {
+            et++;
+        }
+    }
+    printf("alphabet = %d\n", alp);
+    printf("number = %d\n", no);
+    printf("etx = %d\n", et);
+}
+
+
+void main() {
+//    exFunc_01();
+//    exFunc_ex02();
+    int ex = exFunc_ex03(1, 2);
+    printf("sum = %d\n", ex);
+}
+
+// 사용자 정의함수 -> 합을 구해줌
+int exFunc_ex03(int a, int b) {
+    int d;
+    d = a + b;
+    return (d);
+}
