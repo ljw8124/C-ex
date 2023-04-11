85c7047bc6df869ba3a1961cfda5320597352538
이정우 <ljw8124@gmail.com>
2023-04-11 12:06:26 +0900

    C 프로그래밍 과제

---
+++ homework.c
@@ -0,0 +1,8 @@
+//
+// Created by Lee-joungwoo on 2023-04-11.
+//
+#include <stdio.h>          // 컴파일 되기 전에 처리(전처리)
+void main() {               // main 함수 정의 return 값은 없음
+    printf("Hello~ \n");    // Hello 문자열 출력, 그리고 줄바꿈
+}                           // main 함수 범위
+
