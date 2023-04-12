64e27cf160c6287794e57c1f4a39bdb1d08dab04
이정우 <ljw8124@gmail.com>
2023-04-12 12:40:39 +0900

    포인터 기본이론 및 예제

---
+++ pointerEx.c
@@ -0,0 +1,34 @@
+//
+// Created by asoosoft10 on 2023-04-12.
+//
+
+#include <stdio.h>
+void main() {
+//    int *p, i = 3, j;
+//    p = &i;     // 포인터 변수 p 는 변수 i 의 주소를 가리킴
+//    j = *p;     // 포인터 변수 p 가 가리키는 번지의 내용을 변수 j 에 대입
+//    j++;        // 변수 j 의 값을 1 증가
+//    printf("*p = %d\n", *p);
+//    printf("p = %x\n", p);
+//    printf("j = %d\n", j);
+//
+//    int a = 100;
+//    char b = 'b';
+//    void *q = NULL;
+//    q = (int*)&a;
+//    printf("*q = %d\n", *(int*)q);
+//    q = (char*)&b;
+//    printf("*q = %c\n", *(char*)q);
+
+    int *p, a[] = {10, 20, 30, 40, 50};
+    p = &a[0];
+    printf("*p == %d\n", *p);
+    printf("*p++ == %d\n", *p++);
+    printf("*++p == %d\n", *++p);
+
+    p = p + 2;
+    printf("*p == %d\n", *p);
+    printf("a[2] == %d\n", a[2]);
+    printf("*p + 2 == %d\n", *p + 2);
+
+}
