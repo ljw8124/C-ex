951fab96e08c733080f0267d78fccb874c74fe56
이정우 <ljw8124@gmail.com>
2023-04-14 10:28:35 +0900

    2-3 예제 - 헤더파일을 이용하여 함수 호출

---
+++ exam2-3.h
@@ -0,0 +1,8 @@
+
+// 함수 재사용을 위한 헤더파일 생성
+
+#include <stdio.h>              // printf 사용위한 선언
+#define ADD(x, y) ((x) + (y))   // 특정 연산을 위해 ADD 라는 매크로를 정의
+#define SUB(x, y) ((x) - (y))   // 특정 연산을 위해 SUB 라는 매크로를 정의
+
+
